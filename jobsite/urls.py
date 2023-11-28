eff798663bb18d251eb10bec774660e9bc3bc38a
kevin-ci <kevin@codeinstitute.net>
2023-11-28 18:38:41 +0000

    feat: add basic profile template

--- jobsite/urls.py
+++ jobsite/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path("", include("home.urls")),
     path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
+    path('profiles/', include("profiles.urls")),
 ]
