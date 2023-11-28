1fd06ce924edf86aae706d26e0343a4389dee086
kevin-ci <kevin@codeinstitute.net>
2023-11-28 18:27:53 +0000

    feat: setup signal to create profiles on user creation

--- jobsite/settings.py
+++ jobsite/settings.py
@@ -116,6 +116,10 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_UNIQUE_EMAIL = True
+ACCOUNT_USERNAME_REQUIRED = False
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
