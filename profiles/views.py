dfa4a975594873fbf27728d7782b10682da47de3
kevin-ci <kevin@codeinstitute.net>
2023-11-28 19:20:01 +0000

    feat: setup basic profile editing

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.contrib.auth.decorators import login_required
 from .models import UserProfile
+from .forms import UserProfileForm
 
 # Create your views here.
-
 
 @login_required
 def profile(request):
@@ -11,6 +11,28 @@ def profile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
     template = 'profiles/profile.html'
     context = {
+        'profile': profile,
+    }
+
+    return render(request, template, context)
+
+@login_required
+def edit_profile(request):
+    profile = get_object_or_404(UserProfile, user=request.user)
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            # messages.success(request, 'Your profile has been updated!')
+            return redirect(reverse('profile'))
+        # else:
+            # messages.error(request, ('Profile update failed.'))
+    else:
+        form = UserProfileForm(instance=profile)
+
+    template = 'profiles/edit_profile.html'
+    context = {
+        'form': form,
         'profile': profile,
     }
 
