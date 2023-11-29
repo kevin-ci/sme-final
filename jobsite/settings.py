ba1dca940e202a1076c1c3ebe20bfa26e0a265a1
kevin-ci <kevin@codeinstitute.net>
2023-11-29 20:43:36 +0000

    fix: add missing auth backends to settings.py

--- jobsite/settings.py
+++ jobsite/settings.py
@@ -35,6 +35,14 @@ CSRF_TRUSTED_ORIGINS = [
 
 # Application definition
 
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+    # `allauth` specific authentication methods, such as login by email
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+ 
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
