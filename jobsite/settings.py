080d84d07d9eab86376e00d9bb21fc14e039be8a
kevin-ci <kevin@codeinstitute.net>
2023-11-29 16:12:48 +0000

    feat: add username and headline to signup form

--- jobsite/settings.py
+++ jobsite/settings.py
@@ -147,3 +147,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ACCOUNT_SIGNUP_FORM_CLASS = 'profiles.forms.SignupForm'
--- profiles/forms.py
+++ profiles/forms.py
@@ -5,3 +5,13 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         exclude = ('user',)
+        
+class SignupForm(forms.Form):
+    name = forms.CharField(max_length=50, label='Your name')
+    headline = forms.CharField(max_length=50, label='Your profile headline')
+    
+    def signup(self, request, user):
+        user.refresh_from_db()
+        user.profile.name = self.cleaned_data['name']
+        user.profile.heading = self.cleaned_data['headline']
+        user.save()
