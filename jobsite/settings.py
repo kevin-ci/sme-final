e2bd58d22189f546534cb24640f495d9d6010a0b
kevin-ci <kevin@codeinstitute.net>
2023-11-29 14:43:51 +0000

    feat: setup cloudinary and profile images

--- jobsite/settings.py
+++ jobsite/settings.py
@@ -24,13 +24,14 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY') 
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['8000-kevinci-smefinal-0e3zy8q3s0m.ws-eu106.gitpod.io']
-CSRF_TRUSTED_ORIGINS = ['https://8000-kevinci-smefinal-0e3zy8q3s0m.ws-eu106.gitpod.io']
+CSRF_TRUSTED_ORIGINS = [
+    'https://8000-kevinci-smefinal-0e3zy8q3s0m.ws-eu106.gitpod.io']
 
 # Application definition
 
@@ -41,10 +42,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'cloudinary',
     'home',
     'profiles'
 ]
--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class UserProfile(models.Model):
@@ -10,6 +11,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     name = models.CharField(max_length=50, null=True, blank=True)
     heading = models.CharField(max_length=50, null=True, blank=True)
+    profile_image = CloudinaryField('image', default='placeholder')
     
     def __str__(self):
         return self.user.email
